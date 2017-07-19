727f9c7c6f3efceabfd2277c9b467ffd5e3997d1
Kelsey Hyde <kelseyhyde@gmail.com>
2017-07-19 18:13:27 +1200

    Altered test to allow for different numbers of IDs

--- generatortest.py
+++ generatortest.py
@@ -1,11 +1,15 @@
-import idgenerator
+import generator
 import time
+from sys import argv
+
+script, num = argv
+num = int(num)
 
 startTime = time.time()
 idCount = 0
 
-for i in range(0, 1000) :
-    print idgenerator.generateId()
+for i in range(0, num) :
+    print generator.generateId()
     idCount = idCount + 1
 
 endTime = time.time()
